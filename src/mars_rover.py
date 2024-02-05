210bbd5880cfcb45c9902927a88b94c8bb845d17
benlee1284 <ben.lee@kayenta.io>
2024-02-05 16:19:09 +0000

    Refactor: add direction state

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -1,8 +1,16 @@
+NORTH = "N"
+EAST = "E"
+SOUTH = "S"
+WEST = "W"
+
+
 class MarsRover:
     location = [0, 0]
+    direction = NORTH
 
     def move_forward_east(self) -> None:
         self.location[0] += 1
 
     def move_backwards(self) -> None:
-        self.location[1] -= 1
+        if self.direction == NORTH:
+            self.location[1] -= 1
