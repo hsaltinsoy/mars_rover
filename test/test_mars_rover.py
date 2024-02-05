ca825142d270a8b1dfda05716beab6372bf74ea0
saidaltinsoy <said.altinsoy@kayenta.io>
2024-02-05 16:18:37 +0000

    added test for moving forward

--- test/test_mars_rover.py
+++ test/test_mars_rover.py
@@ -1,2 +1,12 @@
+from src.mars_rover import MarsRover
+
+
 def test_dummy():
     assert True == True
+
+
+def test_move_forward_east():
+    rover = MarsRover()
+    rover.move_forward_east()
+    moved_forwarded_location = [1, 0]
+    assert rover.location == moved_forwarded_location
