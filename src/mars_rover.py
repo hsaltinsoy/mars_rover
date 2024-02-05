f3e9da0f574566bfe6a6e9aa95fc64ecc26e11e8
benlee1284 <ben.lee@kayenta.io>
2024-02-05 16:16:59 +0000

    Add move backwards north

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -1,8 +1,8 @@
-
 class MarsRover:
     location = [0, 0]
 
-    def move_forward_east(self):
+    def move_forward_east(self) -> None:
         self.location[0] += 1
 
-
+    def move_backwards(self) -> None:
+        self.location[1] -= 1
--- test/test_mars_rover.py
+++ test/test_mars_rover.py
@@ -1,2 +1,11 @@
+from src.mars_rover import MarsRover
+
+
 def test_dummy():
     assert True == True
+
+
+def test_move_backwards_north():
+    rover = MarsRover()
+    rover.move_backwards()
+    assert rover.location == [0, -1]
