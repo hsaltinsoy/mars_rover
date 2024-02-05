b1a4ecb4c8566ee2e9ec0c66cd4c6e88cd2bdee5
saidaltinsoy <said.altinsoy@kayenta.io>
2024-02-05 16:58:55 +0000

    updated moving forward based on recent changes

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -9,8 +9,15 @@ class MarsRover:
         self.location = location
         self.direction = direction
 
-    def move_forward_east(self) -> None:
-        self.location[0] += 1
+    def move_forward(self) -> None:
+        if self.direction == NORTH:
+            self.location[1] += 1
+        elif self.direction == EAST:
+            self.location[0] += 1
+        elif self.direction == SOUTH:
+            self.location[1] -= 1
+        elif self.direction == WEST:
+            self.location[0] -= 1
 
     def move_backwards(self) -> None:
         if self.direction == NORTH:
--- test/test_mars_rover.py
+++ test/test_mars_rover.py
@@ -7,11 +7,19 @@ def test_dummy():
     assert True == True
 
 
-def test_move_forward_east():
-    rover = MarsRover(location=[0, 0], direction=EAST)
-    rover.move_forward_east()
-    moved_forwarded_location = [1, 0]
-    assert rover.location == moved_forwarded_location
+@pytest.mark.parametrize(
+    "location, direction, expected",
+    [
+        ([0, 0], NORTH, [0, 1]),
+        ([0, 0], EAST, [1, 0]),
+        ([0, 0], SOUTH, [0, -1]),
+        ([0, 0], WEST, [-1, 0]),
+    ],
+)
+def test_move_forward(location: list[int], direction: str, expected: list[int]):
+    rover = MarsRover(location=location, direction=direction)
+    rover.move_forward()
+    assert rover.location == expected
 
 
 @pytest.mark.parametrize(
